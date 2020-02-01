e1d478ba45575fbbb700c6a85375357268ad44a2
Viorie Gerrid <57219804+vg2207@users.noreply.github.com>
2020-02-01 21:16:04 +0700

    submit to algoritma

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 This is Algoritma's Python for Data Analysis Capstone Project. This project aims to create a simple API to fetch data from Heroku Server. 
 
 As a Data Scientist, we demand data to be accessible. And as a data owner, we are careful with our data. As the answer, data owner create an API for anyone who are granted access to the data to collect them. In this capstone project, we will create Flask Application as an API and deploy it to Heroku Web Hosting. 
-
-We provide a brief guideline to create the API and how to Deploy in `Capstone Guideline.ipynb` using Bahasa Indonesia. 
+ 
 ___
 ## Dependencies : 
 - Pandas    (pip install pandas)
@@ -17,25 +16,75 @@ ___
 - Implements the data analysis and wrangling behind the works
 
 ___
-We have deployed a simple example on : https://algo-capstone.herokuapp.com
-Here's the list of its endpoints: 
-```
-1. / , method = GET
-Base Endpoint, returning welcoming string value. 
+We have deployed a simple example on : https://capstone-algoritma.herokuapp.com
 
-2. /data/get/<data_name> , method = GET
-Return full data <data_name> in JSON format. Currently available data are:
-    - books_c.csv
-    - pulsar_stars.csv 
-    
-3. /data/get/equal/<data_name>/<column>/<value> , method = GET
-Return all <data_nam> where the value of column <column> is equal to <value>
+Here's the list of its endpoints: 
+```    
+1. / , method = GET
 ```
+Base Endpoint, returning *welcoming string* value. 
+
+
+```
+2. /capstone/get/<data_name>/rawdata , method = GET
+```
+Return data **<data_name>** in JSON format. Currently available data are:
+- `chinook.db`
+
+The data that can be shown from this link is only part of the `chinook.db` data, which is a combination of **genres**, **tracks**, **media_types**, **invoice_items**, **invoices**, and **customers** dataframe. 
+
+
+```
+3. /capstone/get/<data_name>/rawdata/<column_name>/<value> , method = GET
+```
+Return all **<data_name>** (combination of genres, tracks, media_types, invoice_items, invoices, and customers dataframe) in JSON format, where the value of column **<column_name>** is equal to **<**value**>**. To check the value of **<column_name>** and **<**value**>** that can be used, open the `capstone.ipynb` on your jupyter notebook, visual studio code, or any other IDEs, and try to run the code.
+
+Here is an example for the value that you can use :
+- **<column_name>** : select one of this names [*'GenreName'*, *'GenreId'*, *'TrackName'*, *'AlbumId'*, *'MediaTypeId'*, *'Composer'*, *'Milliseconds'*, *'Bytes'*, *'MediaTypeName'*, *'InvoiceLineId'*, *'InvoiceId'*, *'TrackId'*, *'UnitPrice'*, *'Quantity'*, *'InvoiceDate'*, *'BillingAddress'*, *'BillingCity'*, *'BillingState'*, *'BillingCountry'*, *'BillingPostalCode'*, *'Total'*, *'CustomerId'*, *'FirstName'*, *'LastName'*, *'Company'*, *'Address'*, *'City'*, *'State'*, *'Country'*, *'PostalCode'*, *'Phone'*, *'Fax'*, *'Email'*, *'SupportRepId'*]
+
+- **<**value**>** : if you select *'Company'* as your **<column_name>**, the value of <value> that you can use is one of this values [*'Microsoft Corporation'*, *'Woodstock Discos'*, *'Banco do Brasil S.A.'*, *'Apple Inc.'*, *'Riotur'*, *'Rogers Canada'*, *'JetBrains s.r.o.'*, *'Google Inc.'*, *'Telus'*, *'Embraer - Empresa Brasileira de Aeronáutica S.A.'*]
+
+
+```
+4. /capstone/get/<data_name>/quantity , method = GET
+```
+Return **<data_name>** (combination of genres, tracks, media_types, invoice_items, invoices, and customers dataframe) in JSON format, but not all of the columns from the dataframes are shown.
+
+The columns that are shown :
+
+- **'GenreName'** : from the **genres** dataframe
+- **'MediaTypeName'** : from the **media_types** dataframe
+- **'Quantity'** : from the **invoice_items** dataframe
+- **'InvoiceDate'** : from **invoice** dataframe
+- **'Company'** : from **customers** dataframe
+- **'City'** : from **customers** dataframe
+- **'Country'** : from **customers** dataframe
+- **'InvoiceDay'** : from extracting the name of day in *'InvoiceDate'*
+- **'InvoiceMonth'** : from extracting the month in *'InvoiceDate'*
+- **'InvoiceYearMonth'** : from extracting the year and month in *'InvoiceDate'*
+
+
+```
+5. /capstone/get/<data_name>/quantity/prop/<index>/<columns> , method = GET
+```
+Return a **proportional dataframe** in JSON format, where the data is processed from the endpoint #4 dataframe. The values in the **proportional dataframe** are sums of the *'Quantity'* and then proportionated based on the value of **<**index**>** and **<**columns**>**. You can select two (2) of the values below as the combination of **<**index**>** and **<**columns**>** value :
+
+- **'GenreName'** : from the **genres** dataframe
+- **'MediaTypeName'** : from the **media_types** dataframe
+- **'Company'** : from **customers** dataframe
+- **'City'** : from **customers** dataframe
+- **'Country'** : from **customers** dataframe
+- **'InvoiceDay'** : from extracting the name of day in *'InvoiceDate'* from **invoice** dataframe
+- **'InvoiceMonth'** : from extracting the month in *'InvoiceDate'* from **invoice** dataframe
+- **'InvoiceYearMonth'** : from extracting the year and month in *'InvoiceDate'* from **invoice** dataframe
+
+
+---
 
 If you want to try it, you can access (copy-paste it) : 
-- https://algo-capstone.herokuapp.com
-- https://algo-capstone.herokuapp.com/data/get/books_c.csv
-- https://algo-capstone.herokuapp.com/data/get/pulsar_stars.csv
-- https://algo-capstone.herokuapp.com/data/get/equal/books_c.csv/isbn/0439785960
-- https://algo-capstone.herokuapp.com/data/get/equal/books_c.csv/authors/J.K. Rowling
+- https://capstone-algoritma.herokuapp.com/
+- https://capstone-algoritma.herokuapp.com/capstone/get/chinook.db/rawdata
+- https://capstone-algoritma.herokuapp.com/capstone/get/chinook.db/rawdata/PostalCode/12227-000
+- https://capstone-algoritma.herokuapp.com/capstone/get/chinook.db/quantity
+- https://capstone-algoritma.herokuapp.com/capstone/get/chinook.db/quantity/prop/InvoiceMonth/GenreName
 - and so on, just follow the endpoint's pattern
--- app.py
+++ app.py
@@ -2,40 +2,50 @@ from flask import Flask, request
 import pandas as pd 
 import sqlite3
 
+
 app = Flask(__name__) 
 
-# mendapatkan keseluruhan data dari <data_name>
+
+# 1. Welcoming string
+@app.route('/')
+def home():
+    return "<h1>Hello, Welcome to Viorie's API Capstone</h1>"
+
+
+# 2. Get all the data from <data_name>, but only restricted to genres, tracks, media_types, invoice_items, invoices, and customers dataframe
 @app.route('/capstone/get/<data_name>/rawdata', methods=['GET']) 
 def get_data_raw(data_name): 
     conn = sqlite3.connect("data_input/" + str(data_name))
     query = """
-    SELECT genres.Name as GenreName, genres.GenreId, tracks.Name as TrackName, tracks.AlbumId, tracks.MediaTypeId, tracks.Composer, tracks.Milliseconds, tracks.Bytes, invoice_items.*, invoices.InvoiceDate, invoices.BillingAddress, invoices.BillingCity, invoices.BillingState, invoices.BillingCountry, invoices.BillingPostalCode, invoices.Total, customers.*
+    SELECT genres.Name as GenreName, genres.GenreId, tracks.Name as TrackName, tracks.AlbumId, tracks.MediaTypeId, tracks.Composer, tracks.Milliseconds, tracks.Bytes, media_types.Name as MediaTypeName, invoice_items.*, invoices.InvoiceDate, invoices.BillingAddress, invoices.BillingCity, invoices.BillingState, invoices.BillingCountry, invoices.BillingPostalCode, invoices.Total, customers.*
     FROM genres
     JOIN tracks ON genres.GenreId == tracks.GenreId
+    JOIN media_types ON tracks.MediaTypeId == media_types.MediaTypeId
     JOIN invoice_items ON tracks.TrackId == invoice_items.TrackId
     JOIN invoices ON invoice_items.InvoiceId == invoices.InvoiceId
     JOIN customers ON invoices.CustomerId == customers.CustomerId
     """
 
-    x = pd.read_sql_query(query, conn, parse_dates='InvoiceDate')
+    x = pd.read_sql_query(query, conn, parse_dates="InvoiceDate")
 
     return (x.to_json())
 
 
-
+# 3. Search <column_name> and <value> from <data_name>, but only restricted to genres, tracks, media_types, invoice_items, invoices, and customers dataframe
 @app.route('/capstone/get/<data_name>/rawdata/<column_name>/<value>', methods=['GET']) 
 def get_data_search(data_name, column_name, value): 
     conn = sqlite3.connect("data_input/" + str(data_name))
     query = """
-    SELECT genres.Name as GenreName, genres.GenreId, tracks.Name as TrackName, tracks.AlbumId, tracks.MediaTypeId, tracks.Composer, tracks.Milliseconds, tracks.Bytes, invoice_items.*, invoices.InvoiceDate, invoices.BillingAddress, invoices.BillingCity, invoices.BillingState, invoices.BillingCountry, invoices.BillingPostalCode, invoices.Total, customers.*
+    SELECT genres.Name as GenreName, genres.GenreId, tracks.Name as TrackName, tracks.AlbumId, tracks.MediaTypeId, tracks.Composer, tracks.Milliseconds, tracks.Bytes, media_types.Name as MediaTypeName, invoice_items.*, invoices.InvoiceDate, invoices.BillingAddress, invoices.BillingCity, invoices.BillingState, invoices.BillingCountry, invoices.BillingPostalCode, invoices.Total, customers.*
     FROM genres
     JOIN tracks ON genres.GenreId == tracks.GenreId
+    JOIN media_types ON tracks.MediaTypeId == media_types.MediaTypeId
     JOIN invoice_items ON tracks.TrackId == invoice_items.TrackId
     JOIN invoices ON invoice_items.InvoiceId == invoices.InvoiceId
     JOIN customers ON invoices.CustomerId == customers.CustomerId
     """
 
-    x = pd.read_sql_query(query, conn, parse_dates='InvoiceDate')
+    x = pd.read_sql_query(query, conn, parse_dates="InvoiceDate")
     x = x.astype("str")
 
     mask = x[column_name] == value
@@ -44,40 +54,85 @@ def get_data_search(data_name, column_name, value):
     return (x.to_json())
 
 
-
-# mendapatkan data dengan filter nilai <value> pada kolom <column>
+# 4. Get part of the data from <data_name>, but only restricted to quantity-related data
 @app.route('/capstone/get/<data_name>/quantity', methods=['GET']) 
 def get_data_quantity(data_name): 
     conn = sqlite3.connect("data_input/" + str(data_name))
     query = """
-    SELECT genres.Name as GenreName, invoice_items.Quantity, customers.Country
+    SELECT genres.Name as GenreName, media_types.Name as MediaTypeName, invoice_items.Quantity, invoices.InvoiceDate, customers.Company, customers.City, customers.Country
     FROM genres
     JOIN tracks ON genres.GenreId == tracks.GenreId
+    JOIN media_types ON tracks.MediaTypeId == media_types.MediaTypeId
     JOIN invoice_items ON tracks.TrackId == invoice_items.TrackId
     JOIN invoices ON invoice_items.InvoiceId == invoices.InvoiceId
     JOIN customers ON invoices.CustomerId == customers.CustomerId
     """
 
-    x = pd.read_sql_query(query, conn)
+    x = pd.read_sql_query(query, conn, parse_dates="InvoiceDate")
+    
+    x["InvoiceDay"] = x["InvoiceDate"].dt.weekday_name
+    x["InvoiceDay"] = pd.Categorical(x["InvoiceDay"], categories=['Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday', 'Sunday'], ordered=True)
+
+    x["InvoiceMonth"] = x["InvoiceDate"].dt.month
+    x["InvoiceMonth"] = x["InvoiceMonth"].replace({
+        1 : "January",
+        2 : "February",
+        3 : "March",
+        4 : "April",
+        5 : "May",
+        6 : "June",
+        7 : "July",
+        8 : "August",
+        9 : "September",
+        10 : "October",
+        11 : "November",
+        12 : "December"})
+    x["InvoiceMonth"] = pd.Categorical(x["InvoiceMonth"], categories=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"], ordered=True)
+
+    x["InvoiceYearMonth"] = x["InvoiceDate"].dt.to_period('M')
 
     return (x.to_json())
 
 
-@app.route('/capstone/get/<data_name>/quantity/prop', methods=['GET']) 
-def get_data_prop(data_name): 
+# 5. Get proportional table of quantity from <data_name> with dynamic <index> and <columns>
+@app.route('/capstone/get/<data_name>/quantity/prop/<index>/<columns>', methods=['GET']) 
+def get_data_prop(data_name, index, columns): 
     conn = sqlite3.connect("data_input/" + str(data_name))
     query = """
-    SELECT genres.Name as GenreName, invoice_items.Quantity, customers.Country
+    SELECT genres.Name as GenreName, media_types.Name as MediaTypeName, invoice_items.Quantity, invoices.InvoiceDate, customers.Company, customers.City, customers.Country
     FROM genres
     JOIN tracks ON genres.GenreId == tracks.GenreId
+    JOIN media_types ON tracks.MediaTypeId == media_types.MediaTypeId
     JOIN invoice_items ON tracks.TrackId == invoice_items.TrackId
     JOIN invoices ON invoice_items.InvoiceId == invoices.InvoiceId
     JOIN customers ON invoices.CustomerId == customers.CustomerId
     """
 
-    x = pd.read_sql_query(query, conn)
-    y = (pd.crosstab(index=x["Country"],
-                    columns=x["GenreName"], 
+    x = pd.read_sql_query(query, conn, parse_dates="InvoiceDate")
+    
+    x["InvoiceDay"] = x["InvoiceDate"].dt.weekday_name
+    x["InvoiceDay"] = pd.Categorical(x["InvoiceDay"], categories=['Monday', 'Tuesday','Wednesday','Thursday','Friday','Saturday', 'Sunday'], ordered=True)
+
+    x["InvoiceMonth"] = x["InvoiceDate"].dt.month
+    x["InvoiceMonth"] = x["InvoiceMonth"].replace({
+        1 : "January",
+        2 : "February",
+        3 : "March",
+        4 : "April",
+        5 : "May",
+        6 : "June",
+        7 : "July",
+        8 : "August",
+        9 : "September",
+        10 : "October",
+        11 : "November",
+        12 : "December"})
+    x["InvoiceMonth"] = pd.Categorical(x["InvoiceMonth"], categories=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"], ordered=True)
+
+    x["InvoiceYearMonth"] = x["InvoiceDate"].dt.to_period('M')
+
+    y = (pd.crosstab(index=x[str(index)],
+                    columns=x[str(columns)], 
                     values=x["Quantity"], 
                     aggfunc="sum", 
                     normalize="all", 
@@ -89,35 +144,6 @@ def get_data_prop(data_name):
         astype("str")+"%"
 
     return (y.to_json())
-
-
-@app.route('/capstone/get/<data_name>/quantity/prop/<genre_name>', methods=['GET']) 
-def get_data_prop_genre(data_name, genre_name): 
-    conn = sqlite3.connect("data_input/" + str(data_name))
-    query = """
-    SELECT genres.Name as GenreName, invoice_items.Quantity, customers.Country
-    FROM genres
-    JOIN tracks ON genres.GenreId == tracks.GenreId
-    JOIN invoice_items ON tracks.TrackId == invoice_items.TrackId
-    JOIN invoices ON invoice_items.InvoiceId == invoices.InvoiceId
-    JOIN customers ON invoices.CustomerId == customers.CustomerId
-    """
-
-    x = pd.read_sql_query(query, conn)
-    y = (pd.crosstab(index=x["Country"],
-                    columns=x["GenreName"], 
-                    values=x["Quantity"], 
-                    aggfunc="sum", 
-                    normalize="all", 
-                    margins=True)
-                    *100).\
-        round(2).\
-        sort_values(by="All", ascending=False, axis=1).\
-        sort_values(by="All", ascending=False, axis=0).\
-        astype("str")+"%"
-    z = y[[str(genre_name)]]
-
-    return (z.to_json())
 
 
 if __name__ == '__main__':
